17df11420e5a07f7c35759cfa9d734c5d3287ff4
Isaac Kim <myong328@gmail.com>
2022-04-20 12:53:44 -0700

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Isaac Kim on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
